bd612b36b0f5c79db3c7b1166607712f5e32309e
saliou <gninguecs@outlook.fr>
2022-08-05 03:07:53 -0600

    Ajout d'une nouvelle route /contacts Nouveau port double 'reponse de la vie, de l'univers et du reste' Signed-off-by: saliou <gninguecs@outlook.fr>

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/contacts', contacts);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
