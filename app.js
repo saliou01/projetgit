e4c9eecb8db69c68935c4c4a739e74b013cc9a13
saliou <gninguecs@outlook.fr>
2022-08-10 09:47:47 -0600

    app.js/ extended = true

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: True }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -58,3 +58,5 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
